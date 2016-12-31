dd6107bfeda54ff574b9ff26ce411458d62ab266
uwol <u.wol@wwu.de>
2016-12-31 11:39:02 +0100

    readme

--- README.md
+++ README.md
@@ -9,7 +9,19 @@ This is an example bot for the bot4j framework. It illustrates basic natural lan
 Getting started
 ---------------
 
-* Clone this repo
-* Create a Facebook page, app and webhook (cf. [Facebook Messenger Platform Quick Start](https://developers.facebook.com/docs/messenger-platform/guides/quick-start)).
-* Set config parameters in `facebook.properties`.
-* Deploy to Heroku
+* Clone or download this repo
+* Start bot web server
+  * In [Eclipse](https://eclipse.org) import the directory as a an `existing Maven project`. Right click class `ai.nitro.bot4j.Application`, choose `Run As` and click `Java Application`. A Jetty Web server will start on address `0.0.0.0:4567`.
+  * Alternatively you can start by 
+
+```
+$ mvn clean package exec:java
+```
+
+* Optionally install [ngrok](https://ngrok.com) for exposing the web server on the internet. Start by `ngrok http 4567`.
+* Facebook setup
+   * Create a Facebook page, app, webhook and access token (cf. [Facebook Messenger Platform Quick Start](https://developers.facebook.com/docs/messenger-platform/guides/quick-start)).
+   * Store the Facebook `access token` in file `facebook.properties` and restart the web server.
+* Chat
+  * In Facebook Messenger send a message to your Facebook page. Facebook should forward the message via the configured webhook to your web server.
+  * `ExampleBot` will process the message and answer.
--- src/main/java/ai/nitro/bot4j/ExampleBot.java
+++ src/main/java/ai/nitro/bot4j/ExampleBot.java
@@ -1,29 +1,79 @@
 package ai.nitro.bot4j;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import ai.nitro.bot4j.bot.impl.BotImpl;
 import ai.nitro.bot4j.middle.domain.Participant;
+import ai.nitro.bot4j.middle.domain.receive.payload.PostbackReceivePayload;
 import ai.nitro.bot4j.middle.domain.receive.payload.TextReceivePayload;
+import ai.nitro.bot4j.middle.domain.send.SendMessage;
+import ai.nitro.bot4j.middle.domain.send.button.PostbackSendButton;
+import ai.nitro.bot4j.middle.domain.send.payload.ButtonsSendPayload;
 import ai.nitro.bot4j.nlp.domain.NlpContext;
 
 public class ExampleBot extends BotImpl {
 
+	private static final String BUTTON = "button";
+
 	protected final static Logger LOG = LogManager.getLogger(ExampleBot.class);
+
+	@Override
+	protected void onReceivePostback(final PostbackReceivePayload postback, final Participant sender) throws Exception {
+		final Participant recipient = sender;
+
+		final String name = postback.getName();
+		final String[] payload = postback.getPayload();
+
+		switch (postback.getName()) {
+		case BUTTON:
+			final String joinedPayload = StringUtils.join(payload, ", ");
+			sendText(joinedPayload, recipient);
+			break;
+		default:
+			LOG.warn("Unknown postback {}", name);
+		}
+	}
 
 	@Override
 	protected void onReceiveText(final TextReceivePayload receiveTextPayload, final Participant sender)
 			throws Exception {
 		final Participant recipient = sender;
 
-		final String text = receiveTextPayload.getText();
 		final NlpContext nlpContext = receiveTextPayload.getNlpContext();
 		final String intent = nlpContext.getIntent();
+		final String text = receiveTextPayload.getText();
 
 		LOG.info("received {} with intent {}", text, intent);
 
-		sendText("received '" + text + "' with intent " + intent, recipient);
+		switch (text) {
+		case BUTTON:
+			sendButton(text, recipient);
+			break;
+		default:
+			sendText("received '" + text + "' with intent " + intent, recipient);
+			break;
+		}
+	}
+
+	protected void sendButton(final String title, final Participant recipient) {
+		final SendMessage sendMessage = new SendMessage();
+		sendMessage.setRecipient(recipient);
+
+		final ButtonsSendPayload buttonsSendPayload = new ButtonsSendPayload();
+		buttonsSendPayload.setTitle("some button");
+
+		{
+			final PostbackSendButton button = new PostbackSendButton();
+			button.setTitle(title);
+			button.setName(BUTTON);
+			button.setPayload("test_payload");
+			buttonsSendPayload.addButton(button);
+		}
+
+		sendMessage.addPayload(buttonsSendPayload);
+		messageSender.send(sendMessage);
 	}
 
 }
