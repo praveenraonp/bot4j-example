4824d32c3831b90608fd3c6c5cef48cde47cf21d
uwol <u.wol@wwu.de>
2017-01-30 14:03:48 +0100

    added integrations

--- src/main/java/ai/nitro/bot4j/Application.java
+++ src/main/java/ai/nitro/bot4j/Application.java
@@ -20,21 +20,25 @@ import ai.nitro.bot4j.integration.facebook.receive.webhook.FacebookWebhook;
 import ai.nitro.bot4j.integration.slack.receive.webhook.SlackActionWebhook;
 import ai.nitro.bot4j.integration.slack.receive.webhook.SlackEventWebhook;
 import ai.nitro.bot4j.integration.slack.receive.webhook.SlackOAuthWebhook;
+import ai.nitro.bot4j.integration.telegram.receive.webhook.TelegramWebhook;
 
 public class Application {
 
 	public static void main(final String[] args) {
 		final Injector injector = Guice.createInjector(new Module());
 
+		final AlexaWebhook alexaWebhook = injector.getInstance(AlexaWebhook.class);
 		final FacebookWebhook facebookWebhook = injector.getInstance(FacebookWebhook.class);
 		final SlackActionWebhook slackActionWebhook = injector.getInstance(SlackActionWebhook.class);
 		final SlackEventWebhook slackEventWebhook = injector.getInstance(SlackEventWebhook.class);
 		final SlackOAuthWebhook slackOAuthWebhook = injector.getInstance(SlackOAuthWebhook.class);
-		final AlexaWebhook alexaWebhook = injector.getInstance(AlexaWebhook.class);
+		final TelegramWebhook telegramWebhook = injector.getInstance(TelegramWebhook.class);
 
 		if (System.getenv("PORT") != null) {
 			port(Integer.valueOf(System.getenv("PORT")));
 		}
+
+		post("/alexa", (req, res) -> alexaWebhook.post(req.raw(), res.raw()));
 
 		get("/facebook", (req, res) -> facebookWebhook.get(req.raw(), res.raw()));
 		post("/facebook", (req, res) -> facebookWebhook.post(req.raw(), res.raw()));
@@ -45,6 +49,6 @@ public class Application {
 
 		post("/slack/event", (req, res) -> slackEventWebhook.post(req.raw(), res.raw()));
 
-		post("/alexa", (req, res) -> alexaWebhook.post(req.raw(), res.raw()));
+		post("/telegram", (req, res) -> telegramWebhook.post(req.raw(), res.raw()));
 	}
 }
